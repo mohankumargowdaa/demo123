7fff60834c11f28c95e65f793b99eb18aed08091
HARSHA GOWDA <HARSHA GOWDA@DESKTOP-JGLRBKR>
2021-12-31 19:33:28 +0530

    asd

---
+++ DEMO/src/test/java/MobileTest/powerButtonTest.java
@@ -0,0 +1,11 @@
+package MobileTest;
+
+import org.testng.annotations.Test;
+
+public class powerButtonTest {
+
+	@Test
+public void powerOnTest() {
+		System.out.println("SwitchON the Mobile");
+	}
+}
---
+++ DEMO/src/test/java/MobileTest/volumeButtonTest.java
@@ -0,0 +1,11 @@
+package MobileTest;
+
+import org.testng.annotations.Test;
+
+public class volumeButtonTest {
+
+	@Test
+	public void volumeUpButtonTest() {
+		System.out.println("Volume Up");
+	}
+}
